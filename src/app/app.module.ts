d17e5641bf22358eb27240717bdd1665f0061354
Rodolphe <dutchr@live.fr>
2019-09-05 16:38:01 +0200

    add weather-card & add-card components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { AddCardComponent } from './components/add-card/add-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    WeatherCardComponent,
+    AddCardComponent
   ],
   imports: [
     BrowserModule,
